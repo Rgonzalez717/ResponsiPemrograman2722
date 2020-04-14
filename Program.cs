4ded3ba7d9156df5d873c2b98d0d1100290b9215
Rgonzalez717 <61915917+Rgonzalez717@users.noreply.github.com>
2020-04-14 10:43:07 +0700

    main program , constructor

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2722
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("NIK\t\tNama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------------------");
+            Karyawan karyawan = new Karyawan("19701234", "Agus", 5000000);
+            Karyawan karyawan2= new Karyawan("20040678", "Budi", 7500000);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Asyiiiik!!! Kenaikan Gaji 10%");
+            Console.WriteLine("\n");
+            Console.WriteLine("NIK\t\tNama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------------------");
+
+            karyawan.NaikGaji();
+            karyawan2.NaikGaji();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
