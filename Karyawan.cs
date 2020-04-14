323912075ce99e0848b99f79a1c2b3f6edba89af
Rgonzalez717 <61915917+Rgonzalez717@users.noreply.github.com>
2020-04-14 10:47:47 +0700

    membuat class karyawan , dan naik gaji

---
+++ Karyawan.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2722
+{
+    class Karyawan
+    {
+        public string NIK;
+        public string Nama;
+        public int Gaji;
+
+        public Karyawan(string NIK, string Nama, int Gaji)
+        {
+            this.NIK = NIK;
+            this.Nama = Nama;
+            this.Gaji= Gaji;
+
+            if (Gaji < 0)
+            {
+               
+                Console.WriteLine("{0}\t{1}\t\t0", NIK, Nama);
+            }
+            else
+            {
+               
+                Console.WriteLine("{0}\t{1}\t\t{2}", NIK, Nama, Gaji);
+
+               
+            }
+        }
+        
+        public void NaikGaji()
+        {
+            string NIK = this.NIK;
+            string Nama= this.Nama;
+            int Gaji = this.Gaji;
+           
+            int Bonus = Gaji / 10;
+            int KenaikanGaji = Gaji + Bonus;
+            if(Gaji < 0)
+            {
+
+            Console.WriteLine("{0}\t{1}\t\t0", NIK, Nama);
+            }
+
+            else
+            {
+            Console.WriteLine("{0}\t{1}\t\t{2}", NIK, Nama, KenaikanGaji);
+            }
+        }
+
+    }
+}
